b0c5a3099825d99f69eeb18954693b06b03f8d06
kliptak2 <91429418+kliptak2@users.noreply.github.com>
2022-07-06 01:04:59 -0400

    fetch

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // Your code here
-//API key: 1c882668d9c2c1fecdb66c8653ee2c47
 var form = document.querySelector("#weather-app form");
 var weatherEl = document.getElementById("weather");
 var data = document.getElementById("weather-search");
+var btn = document.querySelector("button")
+var apiKey = "1c882668d9c2c1fecdb66c8653ee2c47"
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${apiKey}&q=${userQuery}"
+var queryString = "?units=imperial&appid={apiKey}" + userQuery
+var fetchUrl = weatherUrl + queryString
+var h2 = document.createElement("h2")
 
-button.onclick = function (e){
+btn.onclick = function (e){
     e.preventDefault();
-    var userQuery = data.value;
-    if (!userQuery) return;
+    var userQuery = data.value
+    if (!userQuery) return
+    fetch(weatherUrl)
+    .then(function(res){
+        if (res.status !== 200){
+            throw new Error("Location not found")
+        }
+        return res.json()
+    })
+    .then(renderWeather)
+    .catch(function(err){
+        weatherEl.innerHTML = err.message
+    })
+}
+
+function renderWeather(){
+
 }
