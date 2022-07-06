692327377d0ba9d2a657818b3673675ec440ba50
kliptak2 <91429418+kliptak2@users.noreply.github.com>
2022-07-05 21:57:10 -0400

    global variables and button.onclick

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Your code here
 //API key: 1c882668d9c2c1fecdb66c8653ee2c47
+var form = document.querySelector("#weather-app form");
+var weatherEl = document.getElementById("weather");
+var data = document.getElementById("weather-search");
+
+button.onclick = function (e){
+    e.preventDefault();
+    var userQuery = data.value;
+    if (!userQuery) return;
+}
